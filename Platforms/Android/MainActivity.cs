8f2782ac611a6c401a08dab0b3bd588f492a9e1a
Asumi <ronan.chaubell@gmail.com>
2024-08-23 13:24:23 +0200

    23/30/2024

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -8,5 +8,14 @@ namespace ArtifactsMMO_Utility_Mobile
     [Activity(MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            if (this.Window != null)
+            {
+                this.Window.RequestFeature(WindowFeatures.NoTitle);
+            }
+            Window.SetFlags(Android.Views.WindowManagerFlags.Fullscreen, Android.Views.WindowManagerFlags.Fullscreen);
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
